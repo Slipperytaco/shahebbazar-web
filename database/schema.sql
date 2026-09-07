69dc00065e022e63f8df0da7135e11d813c267e8
ari <arifadida4@gmail.com>
2026-09-07 23:50:22 +1000

    updated schema and gitignore to be up to date

--- database/schema.sql
+++ database/schema.sql
@@ -10,13 +10,13 @@ CREATE TABLE categories (
 CREATE TABLE vendors (
     vendor_id SERIAL PRIMARY KEY,
     vendor_name VARCHAR(255) NOT NULL,
-    vendor_email VARCHAR(255) NOT NULL UNIQUE,
-    vendor_password VARCHAR(255) NOT NULL,
-    vendor_phone VARCHAR(20),
+    vendor_phone VARCHAR(20) NOT NULL,
     vendor_address TEXT,
     vendor_city VARCHAR(255),
-    vendor_created_at TIMESTAMP DEFAULT NOW()
+    vendor_created_at TIMESTAMP DEFAULT NOW(),
+    user_id INT REFERENCES users(user_id)
 );
+
 
 -- vendor <---> category many to many relationship
 CREATE TABLE vendor_categories (
@@ -28,24 +28,32 @@ CREATE TABLE vendor_categories (
 -- standard end users: 
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
-    user_name VARCHAR(255) NOT NULL,
-    user_email VARCHAR(255) NOT NULL UNIQUE,
+    user_name VARCHAR(255),
+    user_email VARCHAR(255) UNIQUE,
     user_password_hash VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL DEFAULT 'customer',
     user_created_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE vendor_listings (
-  listing_id SERIAL PRIMARY KEY,
-  vendor_id INTEGER REFERENCES vendors(vendor_id),
-  title TEXT NOT NULL,
-  description TEXT,
-  price NUMERIC(10,2),
-  category TEXT,
-  created_at TIMESTAMP DEFAULT NOW()
+    listing_id SERIAL PRIMARY KEY,
+    vendor_id INT REFERENCES vendors(vendor_id),
+    title TEXT NOT NULL,
+    description TEXT,
+    price NUMERIC(10,2),
+    category TEXT,
+    created_at TIMESTAMP DEFAULT NOW()
 );
 
+
 CREATE TABLE listing_photos (
-  photo_id SERIAL PRIMARY KEY,
-  listing_id INTEGER REFERENCES vendor_listings(listing_id),
-  photo_url TEXT NOT NULL
+    photo_id SERIAL PRIMARY KEY,
+    listing_id INT REFERENCES vendor_listings(listing_id),
+    photo_url TEXT NOT NULL
+);
+
+CREATE TABLE vendor_categories (
+    vendor_id INT REFERENCES vendors(vendor_id) ON DELETE CASCADE,
+    category_id INT REFERENCES categories(category_id) ON DELETE CASCADE,
+    PRIMARY KEY (vendor_id, category_id)
 );
