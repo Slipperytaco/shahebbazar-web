3a6af2159f87b89a428fa2d0996333cf249c6e6b
Januul <105201659@student.swin.edu.au>
2026-09-21 16:34:07 +1000

    Revert "Janul dev"

--- 1st-sprint-demo-listings-and-data/files/frontend/shahebbazar/app/(provider)/vendors/dashboard/add-listing/page.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import ListingForm from "../ListingForm";
-
-// The form lives in ListingForm, shared with the inline editor.
-export default function AddListing({ vendorId, onListingCreated }) {
-    return <ListingForm vendorId={vendorId} onSaved={onListingCreated} />;
-}
